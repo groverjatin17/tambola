c887c2d69be4cc649cfb773c875087ad4afa8e38
groverjatin17 <groverjatin17@gmail.com>
2020-08-05 22:41:47 +0530

    firebase integration

--- src/App.js
+++ src/App.js
@@ -7,17 +7,24 @@ import Homepage from './components/Homepage';
 
 import './styles/App.css';
 import GuestTambola from './components/GuestTambola';
+import NavigationBar from './components/NavigationBar';
+import Store from './AppReducer';
 
 function App() {
+  console.log('process.env', process.env);
+
   return (
     <div>
-      <Router history={History}>
-        <Route exact path="/" component={Homepage} />
-        <Switch>
-          <Route exact path="/gameon/:gameNumber" component={Tambola} />
-          <Route path="/:id" component={GuestTambola} />
-        </Switch>
-      </Router>
+      <Store>
+        <Router history={History}>
+          <NavigationBar />
+          <Route exact path="/" component={Homepage} />
+          <Switch>
+            <Route exact path="/gameon/:gameNumber" component={Tambola} />
+            <Route path="/:id" component={GuestTambola} />
+          </Switch>
+        </Router>
+      </Store>
     </div>
   );
 }
